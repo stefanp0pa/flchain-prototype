079943ac32e3ecdad8fcb52a6d0b3044798f7db6
Stefan Popa <andrei.popa2199@gmail.com>
2024-01-15 13:08:26 +0200

    Blockchain: support evaluation event

--- contracts/flchain_dummy/src/flchain_dummy.rs
+++ contracts/flchain_dummy/src/flchain_dummy.rs
@@ -133,17 +133,6 @@ pub trait FlchainDummy {
         return count > 0;
     }
 
-    // #[endpoint]
-    // fn unsignup(&self) {
-    //     require!(
-    //         !self.active_session_manager().is_empty(),
-    //         "Cannot signup! No training session ongoing!"
-    //     );
-
-    //     let session_manager = self.active_session_manager().get();
-    //     let caller_addr = self.blockchain().get_caller();
-    //     let session_id = session_manager.session_id;     
-    // }
 
     fn clear_round_entities(&self, session_id: u64) {
         let max_version = self.version(session_id).get();
@@ -265,6 +254,8 @@ pub trait FlchainDummy {
             self.training_started_event(session_id);
         } else if round == 3u8 {
             self.aggregation_started_event(session_id);
+        } else if round == 4u8 {
+            self.evaluation_started_event(session_id);
         }
     }
 
@@ -328,103 +319,10 @@ pub trait FlchainDummy {
         result
     }
 
-    // #[view]
-    // fn retrieve_client_id_by_address(&self, address: ManagedBuffer) -> u32{
-    //     require!(
-    //         !self.client_by_ipfs_address(&address).is_empty(),
-    //         "This address has not been inserted before!"
-    //     );
-
-    //     self.client_by_ipfs_address(&address).get()
-    // }
-
-    // #[view]
-    // fn retrieve_address_by_client_id(&self, client_id: u32) -> ManagedBuffer{
-    //     require!(
-    //         !self.ipfs_address_by_client(&client_id).is_empty(),
-    //         "This client id has not been inserted before!"
-    //     );
-
-    //     self.ipfs_address_by_client(&client_id).get()
-    // }
-
-    // #[endpoint]
-    // fn signup_trainer(&self, job_hash: ManagedBuffer) {
-    //     let caller = self.blockchain().get_caller();
-    //     self.trainers(&job_hash).insert(caller);
-    // }
-
-    // #[endpoint]
-    // fn remove_trainer(&self, job_hash: ManagedBuffer) -> bool {
-    //     let caller = self.blockchain().get_caller();
-    //     self.trainers(&job_hash).swap_remove(&caller)
-    // }
-
-    // #[view]
-    // fn get_string_vector(&self) -> ManagedVec<ManagedBuffer> {
-    //     let mut result: ManagedVec<ManagedBuffer> = ManagedVec::new();
-    //     // result.push(ManagedBuffer::from("Hello"));
-    //     // result.push(ManagedBuffer::from("World"));
-    //     // result
-    //     self.local_updates(10u64, 21u32).insert(ManagedBuffer::from("Helloooo"));
-    //     self.local_updates(10u64, 21u32).insert(ManagedBuffer::from("World"));
-    //     self.local_updates(10u64, 22u32).insert(ManagedBuffer::from("cococ"));
-    //     for update in self.local_updates(10u64, 21u32).iter() {
-    //         result.push(update);
-    //     }
-    //     result
-    // }
-
-    // #[view]
-    // fn iterate_trainers(&self, job_hash: ManagedBuffer) -> ManagedBuffer {
-    //     let result: ManagedBuffer = ManagedBuffer::new();
-    //     for trainer in self.trainers(&job_hash).iter() {
-    //         let another = trainer.as_managed_buffer();
-    //         result.clone().concat(ManagedBuffer::from(" | "));
-    //         result.clone().concat(another.clone());
-    //     };
-    //     result
-    // }
-
-
     #[view]
     fn get_timestamp(&self) -> u64 {
         self.blockchain().get_block_timestamp()
     }
-
-    // #[endpoint]
-    // fn set_ipfs_file(&self, address: ManagedBuffer, client_id: u32) {
-    //     self.client_by_ipfs_address(&address).set(client_id);
-    //     self.ipfs_address_by_client(&client_id).set(address.clone());
-    // }
-
-    // #[endpoint]
-    // fn set_genesis_address(&self, address: ManagedBuffer) {
-    //     self.genesis_address().set(&address);
-    // }
-
-    // #[endpoint]
-    // fn set_global_version_address(&self, address: ManagedBuffer) {
-    //     self.global_model_version().set(&address);
-    // }
-
-    // #[view]
-    // fn get_genesis_address(&self) -> ManagedBuffer{
-    //     require!(
-    //         !self.genesis_address().is_empty(),
-    //         "This genesis address has not been inserted before!"
-    //     );
-    //     self.genesis_address().get()
-    // }
-
-    // #[view]
-    // fn get_global_version_address(&self) -> ManagedBuffer{
-    //     require!(
-    //         !self.global_model_version().is_empty(),
-    //         "There is no global version available!"
-    //     );
-    //     self.global_model_version().get()
-    // }
 
     // Events.............................................
 
@@ -471,6 +369,12 @@ pub trait FlchainDummy {
         #[indexed] session_id: u64
     );
 
+    #[event("evaluation_started_event")]
+    fn evaluation_started_event(
+        &self,
+        #[indexed] session_id: u64
+    );
+
     // ----------------------------------------------------
 
     #[storage_mapper("active_session_manager")]
@@ -501,20 +405,4 @@ pub trait FlchainDummy {
 
     #[storage_mapper("version")]
     fn version(&self, session_id: u64) -> SingleValueMapper<u32>;
-
-    // #[storage_mapper("trainers")]
-    // fn trainers(&self, job_hash: &ManagedBuffer) -> UnorderedSetMapper<ManagedAddress>;
-
-    // #[storage_mapper("caller_storage")]
-    // fn caller_storage(&self) -> SingleValueMapper<ManagedAddress>;
-
-    
-
-    
-
-    // #[storage_mapper("client_by_ipfs_address")]
-    // fn client_by_ipfs_address(&self, address: &ManagedBuffer) -> SingleValueMapper<u32>;
-
-    // #[storage_mapper("ipfs_address_by_client")]
-    // fn ipfs_address_by_client(&self, client_id: &u32) -> SingleValueMapper<ManagedBuffer>;
 }   
